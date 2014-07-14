ec88fccf11adb9e732f5a52a192b483fac6f7433
Aleksandr <samaydanov@gmail.com>
2014-07-14 11:47:36 -0400

    fix day logic

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,7 @@ class LessonsController < ApplicationController
 	include LessonsHelper
 
 	def day
-		@current_week = get_week_and_day[0]
+		@current_week = get_week_and_day[0] 
 		@current_day = get_week_and_day[1]
 		@lessons = Lesson.where("day = ? AND start_week <= ? AND end_week >= ?", @current_day, @current_week, @current_week)
 										 .order(number: :asc)
@@ -14,13 +14,8 @@ class LessonsController < ApplicationController
 
 	private 
 		def get_week_and_day
-			day = params[:day].to_i
-			day = day == 0 || day > 19 * 7 ? (what_week? * 7 + Time.now.wday) : day
-			diff = day.divmod 7
-			if diff[1] == 0
-				[diff[0] - 1, 7]
-			else 
-				diff
-			end
+			week = if params[:week].blank? then what_week? else params[:week].to_i end
+			day = if params[:day].blank? then Time.now.wday else params[:day].to_i end
+			[week, day]
 		end
 end
--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,7 +1,23 @@
 module LessonsHelper
 	def what_week?
-    point_week = 9
+    point_week = Settings.date.to_datetime.strftime("%W").to_i
     current_week = Time.now.strftime("%W").to_i
-    current_week - point_week
+    current_week - point_week + 1
+  end
+
+  def prev_date(week, day)
+  	if day <= 1
+  		{ week: week - 1, day: 7 }
+  	else 
+  		{ week: week, day: day - 1 }
+  	end
+  end
+
+  def next_date(week, day)
+  	if day >= 7
+  		{ week: week + 1, day: 1 }
+  	else 
+  		{ week: week, day: day + 1 }
+  	end
   end
 end
