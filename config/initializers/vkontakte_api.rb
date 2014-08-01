116060a6464b0c176a2ec1cd5470f0b7c06d1e07
Aleksandr <samaydanov@gmail.com>
2014-08-01 14:51:02 -0400

    vkontakte config

--- config/initializers/vkontakte_api.rb
+++ config/initializers/vkontakte_api.rb
@@ -2,7 +2,7 @@ VkontakteApi.configure do |config|
   # Authorization parameters (not needed when using an external authorization):
   config.app_id       = '4467674'
   config.app_secret   = 'U9b4WTIdi5PDM3akp6El'
-  config.redirect_uri = 'http://localhost:3000/admin/settings/vk/callback'
+  config.redirect_uri = 'http://su-21.herokuapp.com/admin/settings/vk/callback'
   
   
   # Faraday adapter to make requests with:
